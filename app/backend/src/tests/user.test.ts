070374bf40596453c8e8e615376d780b5cfda2d9
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-17 18:40:06 -0300

    feat: criação dos testes

---
+++ app/backend/src/tests/user.test.ts
@@ -0,0 +1,48 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/Users';
+import IUser from '../interfaces/Username'
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('/login', () => {
+  let chaiHttpResponse: Response;
+
+  const userMock: IUser = {
+    id: 1,
+    email: 'email@test.com',
+    password: '123456',
+    role: 'user',
+    username: 'username',
+  }
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(userMock as User);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('espera que retorne um status 200', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login');
+
+    expect(chaiHttpResponse.status).to.equal(200);
+  });
+
+  it('espera que seja retornado um token', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login');
+
+    expect(chaiHttpResponse.body).to.be.an('object').with.key('token');
+  });
+});
