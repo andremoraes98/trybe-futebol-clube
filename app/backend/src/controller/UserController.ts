6f583e2682474e87382f77f5ad88347cb438e8ca
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-18 18:06:27 -0300

    perf: alteração de função para arrow funtcion

--- app/backend/src/controller/UserController.ts
+++ app/backend/src/controller/UserController.ts
@@ -4,10 +4,10 @@ import { IUserService } from '../services/UserService';
 export default class UserController {
   constructor(private userService: IUserService) {}
 
-  async login(req: Request, res: Response) {
+  login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const token = await this.userService.login({ email, password });
 
     res.status(200).json(token);
-  }
+  };
 }
