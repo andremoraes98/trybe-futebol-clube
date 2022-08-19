a7f684de9ad9dcdb3ce224a24a35f88298ffb9ae
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-19 15:38:15 -0300

    feat: criação da rota team

---
+++ app/backend/src/routes/team.ts
@@ -0,0 +1,12 @@
+import { Request, Response, Router } from 'express';
+import TeamController from '../controller/TeamController';
+import TeamService from '../services/TeamService';
+
+const teamService = new TeamService();
+const teamController = new TeamController(teamService);
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => teamController.getAll(req, res));
+
+export default router;
