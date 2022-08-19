90193667f76c4bdbcd0f45a9d066190cba416a2d
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-19 16:02:33 -0300

    feat: adição do endpoint /:id na rota team

--- app/backend/src/routes/team.ts
+++ app/backend/src/routes/team.ts
@@ -8,5 +8,6 @@ const teamController = new TeamController(teamService);
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => teamController.getAll(req, res));
+router.get('/:id', (req: Request, res: Response) => teamController.getById(req, res));
 
 export default router;
