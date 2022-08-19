24911457539871d27ee90278d1b6a572189f852d
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-19 19:36:13 -0300

    feat: criação da rota match

---
+++ app/backend/src/routes/match.ts
@@ -0,0 +1,12 @@
+import { Request, Response, Router } from 'express';
+import MatchController from '../controller/MatchController';
+import MatchService from '../services/MatchService';
+
+const matchService = new MatchService();
+const mathController = new MatchController(matchService);
+
+const route = Router();
+
+route.get('/', (req: Request, res: Response) => mathController.getAll(req, res));
+
+export default route;
