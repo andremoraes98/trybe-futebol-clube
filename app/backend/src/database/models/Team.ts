f38252391895f3dda15b4bba2e83c359e35e70e0
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-19 15:18:58 -0300

    feat: criação do model teams

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,28 @@
+import { Model, STRING, NUMBER } from 'sequelize';
+import ITeam from '../../interfaces/Team';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Team extends Model implements ITeam {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: NUMBER,
+    primaryKey: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  modelName: 'Teams',
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default Team;
