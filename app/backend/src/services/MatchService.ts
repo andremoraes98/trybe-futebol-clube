4ae7119f9cad43ce7a79b8b68fa54e8175a516ff
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-22 16:32:19 -0300

    feat: criação do updateFinish na camada MatchService

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,9 +1,11 @@
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
+import { CustomError } from '../middleware/errorMiddleware';
 
 export interface IMatchService {
   getAll(): Promise<Match[]>;
   create(match: Match): Promise<Match>;
+  updateFinish(id: number): Promise<void>;
 }
 
 export default class MatchService implements IMatchService {
@@ -34,5 +36,17 @@ export default class MatchService implements IMatchService {
     const result = await Match.create(createMatch);
 
     return result;
+  };
+
+  updateFinish = async (id: number) => {
+    if (Number.isNaN(id)) {
+      throw new CustomError('ValidationError', 'Id must be a number');
+    }
+
+    await Match.update({ inProgress: false }, {
+      where: {
+        id,
+      },
+    });
   };
 }
