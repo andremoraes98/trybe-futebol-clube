caa7805434d8ce668de1d133d6f29265cc7a24c3
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-22 17:55:27 -0300

    feat: implementação do getById no UserServices

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -2,12 +2,14 @@ import User from '../database/models/User';
 import Jwt from '../jwt/Jwt';
 import { UserCredential, Token } from '../interfaces/Username';
 import Payload from '../interfaces/Payload';
+import { CustomError } from '../middleware/errorMiddleware';
 
 const jwt = new Jwt();
 
 export interface IUserService {
   login(credentials: UserCredential): Promise<Token>,
   validate(token: string): Promise<string>,
+  getById(id: number): Promise<User>,
 }
 
 export default class UserService implements IUserService {
@@ -25,7 +27,7 @@ export default class UserService implements IUserService {
       email: user.email,
     };
 
-    const token = await jwt.encode(payload);
+    const token = jwt.encode(payload);
 
     return { token };
   };
@@ -41,5 +43,20 @@ export default class UserService implements IUserService {
     }) as User;
 
     return user.role;
+  };
+
+  getById = async (id: number): Promise<User> => {
+    if (Number.isNaN(id)) {
+      throw new CustomError('ValidationError', 'Id must be a number');
+    }
+
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+      raw: true,
+    }) as User;
+
+    return user;
   };
 }
