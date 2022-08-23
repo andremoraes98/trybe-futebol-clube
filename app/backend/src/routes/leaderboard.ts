dc9c208872e87cf10847ed256eae7702b4e91948
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-23 19:12:58 -0300

    feat: implementação da rota /

--- app/backend/src/routes/leaderboard.ts
+++ app/backend/src/routes/leaderboard.ts
@@ -3,13 +3,22 @@ import HomeLeaderController from '../controller/HomeLeaderController';
 import HomeLeaderService from '../services/HomeLeaderService';
 import AwayLeaderController from '../controller/AwayLeaderController';
 import AwayLeaderService from '../services/AwayLeaderService';
+import LeaderController from '../controller/LeaderController';
+import LeaderService from '../services/LeaderService';
 
 const homeLeaderService = new HomeLeaderService();
 const homeLeaderController = new HomeLeaderController(homeLeaderService);
 const awayLeaderService = new AwayLeaderService();
 const awayLeaderController = new AwayLeaderController(awayLeaderService);
+const leaderService = new LeaderService();
+const leaderController = new LeaderController(leaderService);
 
 const route = Router();
+
+route.get(
+  '/',
+  (req: Request, res: Response) => leaderController.getAwayClassification(req, res),
+);
 
 route.get(
   '/home',
