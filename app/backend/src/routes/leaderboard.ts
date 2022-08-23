290a726e33d633e9460b295674b952062250ae3b
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-23 18:18:49 -0300

    feat: implementação da rota /away

--- app/backend/src/routes/leaderboard.ts
+++ app/backend/src/routes/leaderboard.ts
@@ -1,15 +1,24 @@
 import { Request, Response, Router } from 'express';
 import HomeLeaderController from '../controller/HomeLeaderController';
 import HomeLeaderService from '../services/HomeLeaderService';
+import AwayLeaderController from '../controller/AwayLeaderController';
+import AwayLeaderService from '../services/AwayLeaderService';
 
 const homeLeaderService = new HomeLeaderService();
 const homeLeaderController = new HomeLeaderController(homeLeaderService);
+const awayLeaderService = new AwayLeaderService();
+const awayLeaderController = new AwayLeaderController(awayLeaderService);
 
 const route = Router();
 
 route.get(
   '/home',
   (req: Request, res: Response) => homeLeaderController.getHomeClassification(req, res),
+);
+
+route.get(
+  '/away',
+  (req: Request, res: Response) => awayLeaderController.getAwayClassification(req, res),
 );
 
 export default route;
